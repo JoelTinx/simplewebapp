03712429ea6ffd351be37ef2602973024f41908a
Joel Tinx <wizard.joel@gmail.com>
2017-06-07 06:41:04 -0500

    add index controller

--- server.go
+++ server.go
@@ -3,8 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"controllers"
 )
 
 func main()  {
-	fs := http.FileServe()
+	fs := http.FileServer(http.Dir("public"))
+  http.Handle("/public/", http.StripPrefix("/public/", fs))
+
+  http.HandleFunc("/",          controllers.IndexController)
+
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  log.Printf("Listening in port: %s", port)
+  http.ListenAndServe(":" + port, nil)
 }
