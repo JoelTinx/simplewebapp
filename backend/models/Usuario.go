6b8c3f0dcfac13924b162990d51f3897db3a048f
Joel Tinx <wizard.joel@gmail.com>
2017-07-18 21:20:05 -0500

    changes in user model

--- backend/models/Usuario.go
+++ backend/models/Usuario.go
@@ -5,11 +5,33 @@ type User struct {
 	Old int `json:"old"`
 }
 
-func GetAllUser() ([]User, error){
-	conn, err := getConnection()
+func GetAllUser() ([]User, error) {
+	db, err := getConnection()
 	if err != nil {
-		panic(err)
+		log.Fatal(err.Error())
 	}
+	defer db.Close()
+
+	st, err := db.Prepare("call SIS_user_list();")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer st.Close()
+
+	rows, err := st.Query()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer rows.Close()
+
+	users := make([]User, 0)
+	for rows.Next() {
+		var u User
+
+		err = rows.Scan(&u.Name, &u.Old)
+		users = append(users, u)
+	}
+	return users, err
 }
 
 func PostUser() (int, error) {
