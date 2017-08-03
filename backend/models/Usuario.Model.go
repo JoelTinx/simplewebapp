96e38580da8551aa8009c1a9650b669d4d127a4e
Joel Tinx <wizard.joel@gmail.com>
2017-08-02 21:43:45 -0500

    changes in user model

--- backend/models/Usuario.Model.go
+++ backend/models/Usuario.Model.go
@@ -80,10 +80,45 @@ func PutUser() (int, error) {
 	return 1, err
 }
 
-func DeleteUser() (int, error) {
+func DeleteUser(id int) (int, error) {
+	db, err := getConnection()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
 
+	st, err := db.Prepare("call SIS_user_delete();")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer st.Close()
+
+	rows, err := st.Execute(iid)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer rows.Close()
+
+	
+	return 1, err
 }
 
 func GetUser() (User, error) {
+	db, err := getConnection()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
 
+	st, err := db.Prepare("call SIS_user_list();")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer st.Close()
+
+	row := st.QueryRow()
+	var user User
+	row.Scan(&user.Name, &user.Old)
+
+	return user, err
 }
