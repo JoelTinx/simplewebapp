4ba030faff89371d800c985bb7d6d69f5352ffae
Joel Tinx <wizard.joel@gmail.com>
2017-08-16 21:55:23 -0500

    changes in controllers

--- frontend/app/controllers/usuario.controller.js
+++ frontend/app/controllers/usuario.controller.js
@@ -1,1 +1,10 @@
-usuario.controller.js
+angular.module('')
+  .controller('usuarioCrtl', '$http', '$scope', function ($http, $scope) {
+      $scope.usuarios = [];
+      $scope.usuario = {};
+
+      $scope.getUsuarios = function() {
+        
+      };
+      
+  });
