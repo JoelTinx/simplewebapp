1a9d50c5dc5cd48c3579ca310ce23758c034c151
Joel Tinx <wizard.joel@gmail.com>
2017-08-16 21:51:07 -0500

    changes in files

--- backend/server.go
+++ backend/server.go
@@ -12,6 +12,7 @@ func main() {
   // http.Handle("/public/", http.StripPrefix("/public/", fs))
   http.Handle(routes.IndexRoute())
 
+  // Get port of enviroment variables
   port := os.Getenv("PORT")
   if port == "" {
     port = "8080"
--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,3 +2,70 @@ angular.module('myapp', [])
 	.run(function() {
 		console.log('App running');
 	})
+	// DIRECTICA QUE CONVIERTE A MAYUSCULAS EL TEXTO EN UN COMPONENTE
+	.directive('capitalize', function () {
+		return {
+			require: 'ngModel',
+			link: function (scope, element, attrs, modelCtrl) {
+				var capitalize = function (inputValue) {
+					if (inputValue == undefined) inputValue = '';
+					var capitalized = inputValue.toUpperCase();
+					if (capitalized !== inputValue) {
+						modelCtrl.$setViewValue(capitalized);
+						modelCtrl.$render();
+					}
+					return capitalized;
+				}
+				modelCtrl.$parsers.push(capitalize);
+				capitalize(scope[attrs.ngModel]);
+			}
+		};
+	})
+	// DIRECTICA QUE CONVIERTE A MINUSCULAS EL TEXTO EN UN COMPONENTE
+	.directive('uncapitalize', function () {
+		return {
+			require: 'ngModel',
+			link: function (scope, element, attrs, modelCtrl) {
+				var nocapitalize = function (inputValue) {
+					if (inputValue == undefined) inputValue = '';
+					var nocapitalized = inputValue.toLowerCase();
+					if (nocapitalized !== inputValue) {
+						modelCtrl.$setViewValue(nocapitalized);
+						modelCtrl.$render();
+					}
+					return nocapitalized;
+				}
+				modelCtrl.$parsers.push(nocapitalize);
+				nocapitalize(scope[attrs.ngModel]);
+			}
+		};
+	})
+	// DIRECTIVA QUE VALIDA QUE SOLO SE INGRESEN NÚMEROS
+	.directive('validNumber', function () {
+		return {
+			require: '?ngModel',
+			link: function (scope, element, attrs, ngModelCtrl) {
+				if (!ngModelCtrl) {
+					return;
+				}
+
+				ngModelCtrl.$parsers.push(function (val) {
+					if (angular.isUndefined(val)) {
+						var val = '';
+					}
+					var clean = val.replace(/[^0-9]+/g, '');
+					if (val !== clean) {
+						ngModelCtrl.$setViewValue(clean);
+						ngModelCtrl.$render();
+					}
+					return clean;
+				});
+
+				element.bind('keypress', function (event) {
+					if (event.keyCode === 32) {
+						event.preventDefault();
+					}
+				});
+			}
+		};
+	});
